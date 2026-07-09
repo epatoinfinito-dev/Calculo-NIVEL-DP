fdfadd6a275e1df9002b1cfb079c89383d9798dd
epatoinfinito-dev <epatoinfinito@gmail.com>
2026-07-09 17:43:58 -0500

    Primera versión
    primera version de la app creada para realizar los calculos para configurar transmisores de presion por presion diferencial que midan nivel.

---
+++ sw.js
@@ -0,0 +1,14 @@
+var CACHE = "nivel-dp-v1";
+var ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).then(function(){return self.skipWaiting();}));
+});
+self.addEventListener("activate", function(e){
+  e.waitUntil(caches.keys().then(function(ks){
+    return Promise.all(ks.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }).then(function(){return self.clients.claim();}));
+});
+self.addEventListener("fetch", function(e){
+  e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(function(r){return r || fetch(e.request);}));
+});
